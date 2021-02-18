6c02910d12902b50a1e7e7048922acf5486057f9
KeeganMartin <79284080+KeeganMartin@users.noreply.github.com>
2021-02-18 13:16:08 -0800

    Add files via upload

---
+++ Dictionary/Dictionary.h
@@ -0,0 +1,43 @@
+#include "dependencies/LinkedList/LinkedList.h"
+
+template <class T, class U>
+class Dictionary
+{
+private:
+    LinkedList<T> KeyList = LinkedList<T>();
+    LinkedList<U> ValList = LinkedList<U>();
+
+public:
+    void set(T key, U val)
+    {
+        KeyList.add(key);
+        ValList.add(val);
+    }
+
+    U get(T key)
+    {
+        for (int i = 0; i < KeyList.size(); i++)
+        {
+            if (KeyList.get(i) == key)
+            {
+                return ValList.get(i);
+            }
+        }
+    }
+
+    T keyOf(U val)
+    {
+        for (int i = 0; i < ValList.size(); i++)
+        {
+            if (ValList.get(i) == val)
+            {
+                return KeyList.get(i);
+            }
+        }
+    }
+
+    int length()
+    {
+        return KeyList.size();
+    }
+};
